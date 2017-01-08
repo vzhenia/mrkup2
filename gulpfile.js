43181b0d3e88a69becd42a2fff3d411c6d0b3027
vzhenia <vzhenia@bitbucket.org>
2017-01-08 10:59:10 +0200

    images paths fixed

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,5 @@ const gulp = require('gulp');
 const deploy = require('gulp-gh-pages');
 
 gulp.task('deploy', function () {
-  return gulp.src('./dest/*').pipe(deploy())
+  return gulp.src('./dest/**/*').pipe(deploy())
 });
